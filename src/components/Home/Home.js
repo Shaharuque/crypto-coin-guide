6a246ddbeda76ffd1a7135867eca4c1fe6f909b0
unknown <shahrukhamin27@gmail.com>
2022-03-31 22:21:21 +0600

    Home component update commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div className='flex flex-col justify-center items-center h-[80vh]'>
-            <h1 className='font-mono  md:text-4xl'>Getting started with CRYPTO COINS <span className='text-5xl'>☕</span></h1>
+            <div className='md:flex items-center justify-center'>
+                <h1 className='font-mono text-sm md:text-4xl'>Getting started with CRYPTO COINS</h1>
+                <span className='text-5xl flex justify-center'>☕</span>
+            </div>
             <Link className='mt-10 font-sans font-bold text-white hover:text-black bg-teal-600 rounded p-3' to='/coins'>Explore coins</Link>
         </div>
     );
